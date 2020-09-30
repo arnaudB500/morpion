a2a71487746db8794d8112346661a38c68c4f95b
Arnaud Bionde <spacemovy@gmail.com>
2020-09-30 15:13:32 +0200

    commit_2

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,14 @@ public class Main {
 
          int[][] grille = new int [3][3];
          GraphicEngine.print(grille);
-       //  for(int i=0; i< grille.length ; i++) {
-         //    for (int j = 0; j < grille.length; j++) {
-         //        grille[i][j] = new Cell();
-         //        System.out.print(" | " + grille[i][j]);
-          //   }
-          //   System.out.print(" | ");
+       /*for(int i=0; i< grille.length ; i++) {
+             for (int j = 0; j < grille.length; j++) {
+                 grille[i][j] = new Cell();
+                System.out.print(" | " + grille[i][j]);
+             }
+             System.out.print(" | ");
+
+        */
 
 
 
